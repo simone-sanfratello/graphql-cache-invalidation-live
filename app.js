c0c8087c5533fa1945c3757da29bfca7c9c60af0
Simone Sanfratello <simone.sanfratello@nearform.com>
2022-06-01 06:34:27 +0200

    starting

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const fastify = require('fastify')
+const mercurius = require('mercurius')
+const cache = require('mercurius-cache')
+const sleep = require('util').promisify(setTimeout)
+
+const db = require('./lib/db')
+const { request } = require('./lib/util')
+
+async function main () {
+
+}
+
+main()
